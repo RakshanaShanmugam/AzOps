8a13d8714af9df4b7f6e4517ad9b40234d16a5cc
RakshanaShanmugam <88823550+RakshanaShanmugam@users.noreply.github.com>
2021-09-26 22:42:28 +0530

    Update Set-AzOpsWhatIfOutput.ps1

--- src/internal/functions/Set-AzOpsWhatIfOutput.ps1
+++ src/internal/functions/Set-AzOpsWhatIfOutput.ps1
@@ -27,14 +27,15 @@ function Set-AzOpsWhatIfOutput {
         if(-not (Test-Path -Path '/tmp/OUTPUT.md')){
             New-Item -Path '/tmp/OUTPUT.md'
             New-Item -Path '/tmp/OUTPUT.json'
+            Add-Content -Path '/tmp/OUTPUT.md' -Value "WhatIf Results:"
         }
 
         if($removeAzOpsFlag){
-            $mdOutput = '{0}WhatIf Results Resource Deletion:{1}{0}' -f [environment]::NewLine, $results
+            $mdOutput = '{0}Resource Deletion:{1}{0}' -f [environment]::NewLine, $results
         }
         else{
             $resultJson = ($results.Changes | ConvertTo-Json -Depth 5)
-            $mdOutput = 'WhatIf Results:{0}```json{0}{1}{0}```{0}' -f [environment]::NewLine, $resultJson
+            $mdOutput = 'Resource Creation:{0}```json{0}{1}{0}```{0}' -f [environment]::NewLine, $resultJson
             Add-Content -Path '/tmp/OUTPUT.json' -Value $resultJson -WhatIf:$false
         }
         Add-Content -Path '/tmp/OUTPUT.md' -Value $mdOutput -WhatIf:$false
