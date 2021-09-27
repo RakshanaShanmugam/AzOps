307abb15e33bba9cf7e2609a63c00e37208bf3a7
RakshanaShanmugam <88823550+RakshanaShanmugam@users.noreply.github.com>
2021-09-28 01:14:16 +0530

    Update Invoke-AzOpsPush.ps1

--- src/functions/Invoke-AzOpsPush.ps1
+++ src/functions/Invoke-AzOpsPush.ps1
@@ -205,6 +205,7 @@ function Invoke-AzOpsPush {
                     }
                     else{
                         $deleteSetInclusionList += $item
+                        Write-PSFMessage -Level Warning -String 'Invoke-AzOpsPush.Scope.NotFound' -StringValues $deleteSetInclusionList
                     }
                 }
             }
